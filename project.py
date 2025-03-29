a614a694c60df0efc61a38a89f1cd124b80d4c08
Vaidushya <anugya2014@gmail.com>
2025-03-29 21:36:20 +0530

    project file

---
+++ project.py
@@ -0,0 +1,15 @@
+#making summer project
+
+season = input('What season is going on? ')
+
+if(season=='summer'):
+    print("You should wear light weight summer clothes such as cotton T-shirts, shorts, light pants, singlet.")
+
+elif(season=='winter'):
+    print("You should wear heavy warm cloths such as scarf, jackets, hoodies, sweaters.")
+
+elif(season=='monsoon'or'rainy'or'rain'):
+    print("You should wear light weight clothes but not too light that the cold environment can harm you.")
+    
+else:
+    print("You can wear clothes not too warm and not too cold")
